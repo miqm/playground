51c67c804ee91532f027da7e3cb70b47ce022c9b
Miq <7921224+miqm@users.noreply.github.com>
2020-10-31 12:54:55 +0100

    Azure WebApp Docker testing

---
+++ WebApp/WebApplication/WebApi2/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApi2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var text = Environment.GetEnvironmentVariable("TEXT");
+            _logger.LogInformation("TEXT: {text}", text);
+            return Ok($"TEXT: {text}");
+        }
+    }
+}
