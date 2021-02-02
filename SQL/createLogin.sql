7aced78b1ed1912dded95b3db58078548905f5f0
Mikolaj Mackowiak <7921224+miqm@users.noreply.github.com>
2021-02-02 08:17:00 +0100

    Update createLogin.sql

--- SQL/createLogin.sql
+++ SQL/createLogin.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS (SELECT 1 FROM [sys].[database_principals] WHERE [name] = '$(sqlDbUser)') 
+IF NOT EXISTS (SELECT 1 FROM [sys].[sql_logins] WHERE [name] = '$(sqlDbUser)') 
  CREATE USER $(sqlDbUser) WITH PASSWORD='$(sqlDbPassword)';
 ELSE
   ALTER LOGIN $(sqlDbUser) WITH PASSWORD='$(sqlDbPassword)';
